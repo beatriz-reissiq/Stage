f7c0dcb217f489b12361b94817e8be54905524a8
beatriz-reissiq <beatriz.siqueira@sptech.school>
2026-05-31 19:25:18 -0300

    adicionando verificação de idade para postar (correção)*

--- public/js/hubById.js
+++ public/js/hubById.js
@@ -1,156 +1,144 @@
 window.onload = function () {
-    idadeMinima();
-    verificarPerfil();
-    botao();
-}
+  idadeMinima();
+  verificarPerfil();
+  botao();
+};
 
 function idadeMinima() {
+  let dataNasc = new Date(sessionStorage.DATANASC_USUARIO);
+  let dataHoje = new Date();
+  let tempoDeVida = dataHoje.getTime() - dataNasc.getTime();
+  let idade = tempoDeVida / 1000 / 60 / 60 / 24 / 365;
 
-    let dataNasc = new Date(sessionStorage.DATANASC_USUARIO);
-    let dataHoje = new Date();
-    let tempoDeVida = dataHoje.getTime() - dataNasc.getTime();
-    let idade = tempoDeVida / 1000 / 60 / 60 / 24 / 365;
-
-    if (idade < 16) {
-        alert("Você deve ter no mínimo 16 anos para postar!");
-        return false;
-    }
-
+  if (idade < 16) {
+    alert("Você deve ter no mínimo 16 anos para postar!");
+    return false;
+  } else {
     return true;
+  }
 }
 
-
 function verificarPerfil() {
-    var sessaoID = sessionStorage.ID_USUARIO;
-    var genero = sessionStorage.GENERO_USUARIO;
-    var dataNasc = sessionStorage.DATANASC_USUARIO;
+  var sessaoID = sessionStorage.ID_USUARIO;
+  var genero = sessionStorage.GENERO_USUARIO;
+  var dataNasc = sessionStorage.DATANASC_USUARIO;
 
-    if ( sessaoID, genero == undefined ||
-        sessaoID, dataNasc == undefined ) {
-
-        document.getElementById("popupPerfil")
-            .style.display = "flex";
-    } else {
-        document.getElementById("popupPerfil")
-            .style.display = "none";
-    }
+  if ((sessaoID, genero == undefined || sessaoID, dataNasc == undefined)) {
+    document.getElementById("popupPerfil").style.display = "flex";
+  } else {
+    document.getElementById("popupPerfil").style.display = "none";
+  }
 }
 
 function salvarPerfil() {
-    var genero = select_genero.value;
-    var dataNasc = input_idade.value;
-    var id = sessionStorage.ID_USUARIO;
+  var genero = select_genero.value;
+  var dataNasc = input_idade.value;
+  var id = sessionStorage.ID_USUARIO;
 
-    fetch("/usuarios/completarPerfil", {
+  fetch("/usuarios/completarPerfil", {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
 
-        method: "PUT",
-        headers: {
-            "Content-Type": "application/json"
-        },
-
-        body: JSON.stringify({
-            generoServer: genero,
-            dataNascimentoServer: dataNasc,
-            idUsuarioServer: id
-        })
-    })
-
+    body: JSON.stringify({
+      generoServer: genero,
+      dataNascimentoServer: dataNasc,
+      idUsuarioServer: id,
+    }),
+  })
     .then(function () {
+      sessionStorage.GENERO_USUARIO = genero;
+      sessionStorage.DATANASC_USUARIO = dataNasc;
 
-        sessionStorage.GENERO_USUARIO = genero;
-        sessionStorage.DATANASC_USUARIO = dataNasc;
-
-        document.getElementById("popupPerfil")
-            .style.display = "none";
+      document.getElementById("popupPerfil").style.display = "none";
     })
 
     .catch(function (erro) {
-
-        console.log(erro);
+      console.log(erro);
     });
 }
 
-
-
 function botao() {
-    document.getElementById('botaoAdicionar').style.display = 'block';
-};
+  document.getElementById("botaoAdicionar").style.display = "block";
+}
 
 function exibir() {
-    document.getElementById('areaPostagem').style.display = 'flex';
-    document.getElementById('containerPostagem').style.display = 'flex';
+  document.getElementById("areaPostagem").style.display = "flex";
+  document.getElementById("containerPostagem").style.display = "flex";
 }
 
 function fechar() {
-    document.getElementById('areaPostagem').style.display = 'none';
-    document.getElementById('containerPostagem').style.display = 'none';
+  document.getElementById("areaPostagem").style.display = "none";
+  document.getElementById("containerPostagem").style.display = "none";
 }
 
-    function publicar() {
-    var titulo = inputTitulo.value;
-    var descricao = inputDescricao.value;
-    var id = sessionStorage.ID_USUARIO;
+function publicar() {
+  var titulo = inputTitulo.value;
+  var descricao = inputDescricao.value;
+  var id = sessionStorage.ID_USUARIO;
 
-    if (
-    titulo == "" ||
-    descricao == ""
-    ) {
-    
-        alert("Preencha todos os campos!")
-        return false;
+  if (titulo == "" || descricao == "") {
+    alert("Preencha todos os campos!");
+    return false;
   }
 
-     fetch("/postagens/publicar", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
+  if (!idadeMinima()) {
+    return false;
+  }
 
-        body: JSON.stringify({
-            tituloServer: titulo,
-            descricaoServer: descricao,
-            fkUsuarioServer: id
-        })
-    })
+  fetch("/postagens/publicar", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
 
+    body: JSON.stringify({
+      tituloServer: titulo,
+      descricaoServer: descricao,
+      fkUsuarioServer: id,
+    }),
+  })
     .then(function (resposta) {
-        console.log("Post publicado.");
-        document.getElementById('mensagem_post').innerHTML = "Post publicado com sucesso!";
-        setTimeout(function() {
+      console.log("Post publicado.");
+      document.getElementById("mensagem_post").innerHTML =
+        "Post publicado com sucesso!";
+      setTimeout(function () {
         window.location.reload();
-        }, 1500);
+      }, 1500);
 
-       document.getElementById('areaPostagem').style.display = 'none';
-       document.getElementById('containerPostagem').style.display = 'none';
-       document.getElementById('botaoAdicionar').style.display = 'block';
+      document.getElementById("areaPostagem").style.display = "none";
+      document.getElementById("containerPostagem").style.display = "none";
+      document.getElementById("botaoAdicionar").style.display = "block";
 
-        listarPosts();
+      listarPosts();
     })
 
     .catch(function (erro) {
-        document.getElementById('mensagem_post_error').innerHTML = "Erro ao publicar o post!";
-        console.log("Erro ao publicar:", erro);
+      document.getElementById("mensagem_post_error").innerHTML =
+        "Erro ao publicar o post!";
+      console.log("Erro ao publicar:", erro);
     });
-    
-    return false;
+
+  return false;
 }
 
 function listarMeusPosts() {
+  fetch(`/postagens/listarMeusPosts/${sessionStorage.ID_USUARIO}`)
+    .then(function (resposta) {
+      return resposta.json();
+    })
 
-    fetch(`/postagens/listarMeusPosts/${sessionStorage.ID_USUARIO}`)
-        .then(function (resposta) {
-            return resposta.json();
-        })
+    .then(function (posts) {
+      const feed = document.getElementById("feedPosts");
 
-        .then(function (posts) {
-            const feed = document.getElementById('feedPosts');
+      feed.innerHTML = "";
+      for (let i = 0; i < posts.length; i++) {
+        const dataBanco = new Date(posts[i].dataPostagem).toLocaleDateString(
+          "pt-BR",
+        );
 
-            feed.innerHTML = "";
-            for (let i = 0; i < posts.length; i++) {
-
-                const dataBanco = (new Date(posts[i].dataPostagem)).toLocaleDateString('pt-BR');;
-
-               feed.innerHTML += `
+        feed.innerHTML += `
                     
                         <div class="cardPost">
 
@@ -173,12 +161,11 @@ function listarMeusPosts() {
                     </div>
                 <br>
                 `;
-            }
-        })
-        .catch(function (erro) {
-            console.log("Erro ao listar meus posts:", erro);
-        });
+      }
+    })
+    .catch(function (erro) {
+      console.log("Erro ao listar meus posts:", erro);
+    });
 }
 
 listarMeusPosts();
-
