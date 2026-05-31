671b74bc0146e54842ed2a6a6d29ecd42b665706
beatriz-reissiq <beatriz.siqueira@sptech.school>
2026-05-31 17:49:00 -0300

    Função de excluir posts finalizada

--- public/js/dashboard posts.js
+++ public/js/dashboard posts.js
@@ -1,14 +1,74 @@
-function excluirPost(idPostagem) {
+
+function listarPostsAdmin() {
+
+    fetch("/postagens/listar")
+    .then(function(resposta){
+        return resposta.json();
+    })
+
+    .then(function(posts){
+
+        let feed = document.getElementById("feedPosts");
+
+        feed.innerHTML = "";
+
+        for(let i = 0; i < posts.length; i++){
+
+            feed.innerHTML += `
+                <div class="cardPost">
+
+                    <div class="titulo">
+                        ${posts[i].titulo}
+                    </div>
+
+                    <div class="subtitulo">
+                        Autor: ${posts[i].nome}
+                    </div>
+
+                    <div class="descricao">
+                        ${posts[i].descricao}
+                    </div>
+
+                    <button
+                        class="btnExcluir"
+                        onclick="excluirPost(${posts[i].idPostagem})">
+                        Excluir Post
+                    </button>
+
+                </div>
+            `;
+        }
+    })
+
+    .catch(function(erro){
+        console.log(erro);
+    });
+}
+
+function excluirPost(idPostagem){
 
     fetch(`/postagens/excluir/${idPostagem}`, {
         method: "DELETE"
     })
+
     .then(function(resposta){
 
         if(resposta.ok){
-            listarPosts();
-        }
 
+            alerts.style.display = "flex";
+            mensagem_error.innerHTML = "Post excluído com sucesso!";
+
+            setTimeout(() => {
+                alerts.style.display = "none";
+            }, 2500);
+
+            listarPostsAdmin();
+        }
+    })
+
+    .catch(function(erro){
+        console.log(erro);
     });
 }
 
+listarPostsAdmin();
